7054ec097b868b02062d5dbda589d75d394ffd70
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-01-24 08:36:36 +0100

    add: pdo code with comments

--- demo/pdo/datasheet.php
+++ demo/pdo/datasheet.php
@@ -1,28 +1,48 @@
 <?php 
 
+    // Inclut le contenu du fichier header.php (notre connexion bdd et en-tête HTML)
     require('./header.php'); 
     
+    // Récupère l’identifiant du film à partir de l’URL de la page.
     $currentId = $_GET['id'];
     
-    $sql = "SELECT m.id, m.title, m.rating, m.year_released, m.box_office, m.budget, m.duration, d.name AS director, dc.name AS distributeur FROM `movies` AS m INNER JOIN director d ON m.directorID = d.id INNER JOIN distribution_company dc ON m.companyID = dc.id WHERE m.id = " . $currentId;
+    // Requête SQL pour sélectionner les informations du film dans la base de données pour un seul film.
+    // Sélectionne les colonnes de la table movies qui contiennent les détails du film (titre, année de réalisation...)
+    // La clause WHERE spécifie le film que l'on souhaite sélectionner en utilisant son id, il sera récupéré via le $GET_ID dans notre URL et sera assigné à notre variable $currentId.
+    $sql = "SELECT 
+    m.id, 
+    m.title, 
+    m.rating, 
+    m.year_released, 
+    m.box_office, 
+    m.budget, 
+    m.duration, 
+    d.name AS director, 
+    dc.name AS distributeur 
+    FROM `movies` AS m 
+    INNER JOIN director d ON m.directorID = d.id 
+    INNER JOIN distribution_company dc ON m.companyID = dc.id 
+    WHERE m.id = $currentId";
     
+    // Exécute la requête SQL sur la base de données
     $request = $db->query($sql);
-    $movies = $request->fetch();
+
+    // Récupère le résultat de la requête SQL dans un tableau association contenant les informations du film.
+    $movie = $request->fetch();
 
 ?>
 
 <h1>Détails du film</h1>
-<?php foreach ($movies as $movie): ?>
-    <h2><?= $movie['title']; ?></h2>
-    <p>Année de réalisation : <?= $movie['year_released']; ?></p>
-    <p>Par : <?=  $movie['director']; ?></p>
-    <p>De : <?= $movie['distributeur']; ?></p>
-    <p>Note des spectateurs : <?= $movie['rating']; ?></p>
-    <p>Nombres d'entrées : <?=  $movie['box_office']; ?></p>
-    <p>Coût de production : <?=  $movie['budget']; ?></p>
-    <p>Durée : <?=  $movie['duration']; ?></p>
-<?php endforeach; ?>
 
+<!-- Affiche les informations du film. -->
+<h2><?= $movie['title']; ?></h2>
+<p>Année de réalisation : <?= $movie['year_released']; ?></p>
+<p>Par : <?=  $movie['director']; ?></p>
+<p>De : <?= $movie['distributeur']; ?></p>
+<p>Note des spectateurs : <?= $movie['rating']; ?></p>
+<p>Nombres d'entrées : <?=  $movie['box_office']; ?></p>
+<p>Coût de production : <?=  $movie['budget']; ?></p>
+<p>Durée : <?=  $movie['duration']; ?></p>
 
 </body>
 </html>
--- demo/pdo/index.php
+++ demo/pdo/index.php
@@ -1,20 +1,30 @@
 <?php 
 
+    // Inclut le contenu du fichier header.php (notre connexion bdd et en-tête HTML)
     require('./header.php');
 
+    // Requête SQL pour sélectionner toutes les informations des films dans la base de données.
     $sql = "SELECT * FROM `movies`;";
 
+    // Exécute la requête SQL sur la base de données
     $request = $db->query($sql);
+
+    // Récupère le résultat de la requête SQL. Un tableau associatif contenant toutes les informations de tous les films.
     $movies = $request->fetchAll();
 
 ?>
 
 <h1>Les films Utopia</h1>
 
-<?php foreach($movies as $movie): ?>
-    <a href="datasheet.php?id=<?= $film['id'] ?>"><?= $film['title'] ?></a>
-<?php endforeach; ?>
+<!-- Boucle qui parcourt chaque élément du tableau $movies et affiche un lien vers la page de détails pour chaque film. -->
 
+<ul>
+<?php foreach($movies as $movie): ?>
+    <li>
+        <a href="datasheet.php?id=<?= $movie['id'] ?>"><?= $movie['title'] ?></a>
+    </li>
+<?php endforeach; ?>
+</ul>
 
 </body>
 
