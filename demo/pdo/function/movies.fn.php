f31c9d0f1d224d565b35a6445ea2d68322324699
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-01-26 12:22:21 +0100

    add: new getStar function

--- demo/pdo/function/movies.fn.php
+++ demo/pdo/function/movies.fn.php
@@ -73,25 +73,43 @@ function findPictureByMovie($db, $currentId)
 // Définition de la fonction getStar qui prend une note en paramètre
 function getStar($rating)
 {
-    // La note est divisée par 2 et arrondie à un chiffre après la virgule
-    // Cela permet de convertir une note sur 10 à une note sur 5 avec des demi-étoiles
-    $starRating = round($rating / 2, 1);
+    // Arrondit la note du film à l'entier le plus proche.
+    $starRating = round($rating);
 
-    // La note est séparée en deux parties : la partie entière et la partie décimale
-    $ratingInt = explode(".", $starRating);
+    // Divise la note par deux. ex: une note de 9 devient 4.5.
+    $rate = $starRating / 2;
+
+    // Convertit la note en tableau. Ex: 4.5 devient [4,5].
+    // [0] renvoit => 4
+    // [1] renvoit => 5
+    $ratingInt = explode(".", $rate);
+
+    // Initialise le compteur d'étoiles.
+    $starNbr = 0;
 
     // Pour chaque point entier de la note, une étoile pleine est affichée
     for ($i = 0; $i < $ratingInt[0]; $i++) {
+        // Affiche une étoile pleine.
         echo '<div class="bi-star-fill"></div>';
+
+        // Incrémente le compteur d'étoiles.
+        $starNbr++;
     }
 
     // Si la note contient une demi-étoile, elle est affichée
-    if ($ratingInt[1] != 0) {
+    // Le isset vérifie si la deuxième valeur (index 1) dans le tableau $ratingInt existe.
+    if (isset($ratingInt[1])) {
+
+        // Affiche une demi-étoile.    
         echo '<div class="bi-star-half"></div>';
+
+        // Incrémente le compteur d'étoiles.
+        $starNbr++;
     }
 
-    // Si la note est inférieure à 5, une étoile vide est affichée
-    if (5 - $starRating >= 1) {
+    // Affiche des étoiles vides jusqu'à atteindre un total de 5 étoiles.
+    for ($i = 0; $i < 5 -  $starNbr++; $i++) {
+        // Affiche une étoile vide.
         echo '<div class="bi-star"></div>';
     }
 }
