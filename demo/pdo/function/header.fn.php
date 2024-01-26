d4b7d2f46b847de5942bb78ced1abfd772414d9c
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-01-26 14:56:10 +0100

    add: comment isActive function

--- demo/pdo/function/header.fn.php
+++ demo/pdo/function/header.fn.php
@@ -7,12 +7,8 @@ function isActive($page, $url)
     // Si $url est trouvé dans $page, strpos() retourne la position du début de la première occurrence de $url.
     // Sinon, elle retourne FALSE.
     if (strpos($page, $url) !== FALSE) {
-        // Si $url est trouvé dans $page, la fonction echo 'active' est exécutée.
+        // Si $url est trouvé dans $page, la fonction retourne 'active'.
         // Cela signifie que la classe 'active' est ajoutée à l'élément de navigation correspondant.
-        echo 'active';
-    } else {
-        // Si $url n'est pas trouvé dans $page, la fonction echo '' est exécutée.
-        // Cela signifie qu'aucune classe n'est ajoutée à l'élément de navigation.
-        echo '';
+        return 'active text-uppercase';
     }
 }
