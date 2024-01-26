edb6de9500edf949ed204bb2451b2913dc847528
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-01-26 13:38:01 +0100

    add: movies by genre

--- demo/pdo/index-genre.php
+++ demo/pdo/index-genre.php
@@ -42,24 +42,40 @@ require_once __DIR__ . ('/utilities/header.php');
 
 ?>
 
-<h1>Les films Utopia</h1>
+<div class="text-center my-4">
+    <h1>Les films Utopia</h1>
+</div>
 
-<!-- Boucle qui parcourt chaque chaque élément du tableau $genres -->
-<?php foreach ($genres as $genre) : ?>
-    <h2><?= $genre['name']; ?></h2>
 
-    <!-- Récupère tous les films de ce genre -->
-    <?php $movies = findAllMoviesByGenre($db, $genre['name']); ?>
-    <ul>
-        <?php foreach ($movies as $movie) : ?>
-            <li>
-                <a href="<?= $movie_page; ?>?id=<?= $movie['id']; ?>"><?= $movie['title']; ?></a>
-            </li>
+<div class="container">
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+
+        <!-- Boucle qui parcourt chaque chaque élément du tableau $genres -->
+        <?php foreach ($genres as $genre) : ?>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-header bg-black text-white">
+                        <h2 class="card-title"><?= $genre['name']; ?></h2>
+                    </div>
+                    <div class="card-body">
+
+
+                        <!-- Récupère tous les films de ce genre -->
+                        <?php $movies = findAllMoviesByGenre($db, $genre['name']); ?>
+                        <ul class="list-group list-group-flush">
+                            <?php foreach ($movies as $movie) : ?>
+                                <li class="list-group-item">
+                                    <a href="<?= $movie_page; ?>?id=<?= $movie['id']; ?>"><?= $movie['title']; ?></a>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         <?php endforeach; ?>
-    </ul>
-<?php endforeach; ?>
+    </div>
+</div>
 
 </body>
 
 </html>
-
